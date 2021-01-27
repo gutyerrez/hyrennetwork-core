1eb904d694920e8acd2a16b7efe484f7f7c9125f
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-01-27 04:20:16 -0300

    dev

--- core-bungee/src/main/kotlin/com/redefantasy/core/bungee/command/CustomCommand.kt
+++ core-bungee/src/main/kotlin/com/redefantasy/core/bungee/command/CustomCommand.kt
@@ -1,6 +1,32 @@
 package com.redefantasy.core.bungee.command
 
+import com.redefantasy.core.shared.commands.Commandable
+import com.redefantasy.core.shared.users.data.User
+import net.md_5.bungee.api.CommandSender
+import net.md_5.bungee.api.connection.ProxiedPlayer
+import net.md_5.bungee.api.plugin.Command
+import net.md_5.bungee.command.ConsoleCommandSender
+
 /**
-  * @author Gutyerrez
-  */class CustomCommand {
+ * @author Gutyerrez
+ */
+abstract class CustomCommand(
+    name: String
+) : Command(name), Commandable<CommandSender> {
+
+    override fun getSenderName(commandSender: CommandSender): String = commandSender.name
+
+    override fun isPlayer(commandSender: CommandSender) = commandSender is ProxiedPlayer
+
+    override fun isConsole(commandSender: CommandSender) = commandSender is ConsoleCommandSender
+
+    abstract override fun onCommand(commandSender: CommandSender, user: User?, args: Array<out String>): Boolean
+
+    override fun execute(commandSender: CommandSender, args: Array<out String>) {
+        this.executeRaw(
+            commandSender,
+            args
+        )
+    }
+
 }
