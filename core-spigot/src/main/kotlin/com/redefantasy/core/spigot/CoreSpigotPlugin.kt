0f62f9443e3f319d837227ad8502e7129ace6678
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-02-17 20:48:37 -0300

    dev

--- core-spigot/src/main/kotlin/com/redefantasy/core/spigot/CoreSpigotPlugin.kt
+++ core-spigot/src/main/kotlin/com/redefantasy/core/spigot/CoreSpigotPlugin.kt
@@ -1,10 +1,12 @@
 package com.redefantasy.core.spigot
 
 import com.redefantasy.core.shared.CoreProvider
+import com.redefantasy.core.shared.wrapper.CoreWrapper
 import com.redefantasy.core.spigot.command.registry.CommandRegistry
 import com.redefantasy.core.spigot.echo.packets.listener.TitleEchoPacketListener
 import com.redefantasy.core.spigot.misc.commands.TestTitleCommand
 import com.redefantasy.core.spigot.misc.plugin.CustomPlugin
+import com.redefantasy.core.spigot.wrapper.SpigotWrapper
 
 /**
  * @author Gutyerrez
@@ -13,6 +15,8 @@ class CoreSpigotPlugin : CustomPlugin(true) {
 
     override fun onEnable() {
         super.onEnable()
+
+        CoreWrapper.WRAPPER = SpigotWrapper()
 
         CommandRegistry.registerCommand(TestTitleCommand())
 
