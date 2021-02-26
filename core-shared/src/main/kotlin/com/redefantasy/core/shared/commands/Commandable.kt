23950e32465b20b903b055c2022f21c6982c6684
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-02-25 21:14:40 -0300

    dev

--- core-shared/src/main/kotlin/com/redefantasy/core/shared/commands/Commandable.kt
+++ core-shared/src/main/kotlin/com/redefantasy/core/shared/commands/Commandable.kt
@@ -114,9 +114,7 @@ interface Commandable<T> {
                 }
             } else if (this::onCommand.javaMethod?.returnType?.equals(null) == true) {
                 return this.sendAvailableCommands(commandSender, args)
-            } else if (args.isEmpty() && this.getArguments() !== null) {
-                return this.sendAvailableCommands(commandSender, args)
-            } else if (this.getArguments() !== null && args.size < this.getArguments()!!.size) {
+            } else if (args.isEmpty() && this.getArguments() !== null || this.getArguments() !== null && args.size < this.getArguments()!!.size) {
                 return CoreWrapper.WRAPPER.sendMessage(
                         this.getSenderName(commandSender),
                         this.getUsage()
