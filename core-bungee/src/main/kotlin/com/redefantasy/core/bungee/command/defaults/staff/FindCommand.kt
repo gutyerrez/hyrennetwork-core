f9d958daaf3d79c3d7925e83646382d31f872f79
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-02-25 21:21:14 -0300

    dev

--- core-bungee/src/main/kotlin/com/redefantasy/core/bungee/command/defaults/staff/FindCommand.kt
+++ core-bungee/src/main/kotlin/com/redefantasy/core/bungee/command/defaults/staff/FindCommand.kt
@@ -24,15 +24,15 @@ class FindCommand : CustomCommand("find"), GroupCommandRestrictable {
     override fun getDescription() = "Ver informações sobre um usuário."
 
     override fun getArguments() = listOf(
-        Argument("usuário")
+            Argument("usuário")
     )
 
     override fun getGroup() = Group.MANAGER
 
     override fun onCommand(
-        commandSender: CommandSender,
-        user: User?,
-        args: Array<out String>
+            commandSender: CommandSender,
+            user: User?,
+            args: Array<out String>
     ): Boolean {
         val targetUser = CoreProvider.Cache.Local.USERS.provide().fetchByName(args[0])
 
@@ -46,47 +46,48 @@ class FindCommand : CustomCommand("find"), GroupCommandRestrictable {
         val punishments = CoreProvider.Cache.Local.USERS_PUNISHMENTS.provide().fetchByUserId(targetUser.id)
 
         val message = ComponentBuilder()
-            .append("\n")
-            .append("§2Informações sobre o usuário ${targetUser.name}:")
-            .append("\n\n")
-            .append(" ${color}Informações básicas")
-            .append("\n\n")
-            .append("   §fId: §7${targetUser.getUniqueId()}")
-            .append("\n")
-            .append("   §fData de registro: §7${DateFormatter.formatToDefault(targetUser.createdAt)}")
-            .append("\n")
-            .append("   §fÚltima autenticação: §7${DateFormatter.formatToDefault(targetUser.lastLoginAt)}")
-            .append("\n")
-            .append("   §fPunido: ${
-                if (punishments === null || punishments.isEmpty() || punishments.stream()
-                        .filter { it.startTime === null || (it.startTime!! + it.duration).millis > System.currentTimeMillis() && it.revokeCategory === null }
-                        .findFirst()
-                        .isPresent
-                ) {
-                    "§aNão"
-                } else {
-                    "§cSim"
-                }
-            }")
-            .append("\n\n")
-            .append(" ${color}Informações avançadas:")
-            .append("\n\n")
-            .append("   §fEndereço conectado: §7${if (address === null || address.isEmpty()) "§7[Desconhecido]" else address}")
-            .append("\n")
-            .append("   §fÚltimo IP: §7${if (user!!.hasGroup(Group.MANAGER)) targetUser.lastAddress else "§c[Sem permissão]"}")
-            .append("\n\n")
-            .append(" ${color}Associações:")
-            .append("\n\n")
-            .append("   §fE-mail: §7[Não implementado]")
-            .append("\n")
-            .append("   §fDiscord: §7[Não implementado]")
-            .append("\n")
-            .append("   §fTwitter: §7[Não implementado]")
-            .append("\n\n")
-            .append(" ${color}Servidores:")
-            .append("\n\n")
+                .append("\n")
+                .append("§2Informações sobre o usuário ${targetUser.name}:")
+                .append("\n\n")
+                .append(" ${color}Informações básicas")
+                .append("\n\n")
+                .append("   §fId: §7${targetUser.getUniqueId()}")
+                .append("\n")
+                .append("   §fData de registro: §7${DateFormatter.formatToDefault(targetUser.createdAt)}")
+                .append("\n")
+                .append("   §fÚltima autenticação: §7${DateFormatter.formatToDefault(targetUser.lastLoginAt)}")
+                .append("\n")
+                .append("   §fPunido: ${
+                    if (punishments === null || punishments.isEmpty() || punishments.stream()
+                                    .anyMatch {
+                                        it.startTime === null && it.revokeCategory === null || it.isActive()
+                                    }
+                    ) {
+                        "§aNão"
+                    } else {
+                        "§cSim"
+                    }
+                }")
+                .append("\n\n")
+                .append(" ${color}Informações avançadas:")
+                .append("\n\n")
+                .append("   §fEndereço conectado: §7${if (address === null || address.isEmpty()) "§7[Desconhecido]" else address}")
+                .append("\n")
+                .append("   §fÚltimo IP: §7${if (user!!.hasGroup(Group.MANAGER)) targetUser.lastAddress else "§c[Sem permissão]"}")
+                .append("\n\n")
+                .append(" ${color}Associações:")
+                .append("\n\n")
+                .append("   §fE-mail: §7[Não implementado]")
+                .append("\n")
+                .append("   §fDiscord: §7[Não implementado]")
+                .append("\n")
+                .append("   §fTwitter: §7[Não implementado]")
+                .append("\n\n")
+                .append(" ${color}Servidores:")
+                .append("\n\n")
 
-        val groups = CoreProvider.Cache.Local.USERS_GROUPS_DUE.provide().fetchByUserId(targetUser.getUniqueId()) ?: emptyMap()
+        val groups = CoreProvider.Cache.Local.USERS_GROUPS_DUE.provide().fetchByUserId(targetUser.getUniqueId())
+                ?: emptyMap()
 
         var i = 0
 
@@ -95,7 +96,7 @@ class FindCommand : CustomCommand("find"), GroupCommandRestrictable {
             val _groups = it.value
 
             val groupsToString = _groups.stream().map { group -> group.getFancyDisplayName() }
-                .collect(Collectors.joining("§f, "))
+                    .collect(Collectors.joining("§f, "))
 
             message.append("   §f${if (server === null) "Todos" else server.displayName}: $groupsToString")
 
@@ -112,14 +113,14 @@ class FindCommand : CustomCommand("find"), GroupCommandRestrictable {
         val bukkitApplication = CoreProvider.Cache.Redis.USERS_STATUS.provide().fetchBukkitApplication(targetUser)
 
         message.append("\n\n")
-            .append(" ${color}Conexão:")
-            .append("\n\n")
-            .append("   §fConectado: ${if (targetUser.isOnline()) "§aSim" else "§cNão"}")
-            .append("\n")
-            .append("   §fProxy: §7${if (proxyApplication === null) "[Desconhecido]" else proxyApplication.displayName}")
-            .append("\n")
-            .append("   §fServidor: §7${if (bukkitApplication === null || bukkitApplication.server === null) "Nenhum" else bukkitApplication.server!!.displayName}")
-            .append("\n")
+                .append(" ${color}Conexão:")
+                .append("\n\n")
+                .append("   §fConectado: ${if (targetUser.isOnline()) "§aSim" else "§cNão"}")
+                .append("\n")
+                .append("   §fProxy: §7${if (proxyApplication === null) "[Desconhecido]" else proxyApplication.displayName}")
+                .append("\n")
+                .append("   §fServidor: §7${if (bukkitApplication === null || bukkitApplication.server === null) "Nenhum" else bukkitApplication.server!!.displayName}")
+                .append("\n")
 
         commandSender.sendMessage(*message.create())
         return true
