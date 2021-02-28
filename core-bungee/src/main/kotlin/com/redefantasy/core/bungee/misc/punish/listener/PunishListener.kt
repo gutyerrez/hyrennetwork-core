25e10fa9064205cd8732aa840ff5c4e86d372dcf
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-02-28 19:00:31 -0300

    dev

--- core-bungee/src/main/kotlin/com/redefantasy/core/bungee/misc/punish/listener/PunishListener.kt
+++ core-bungee/src/main/kotlin/com/redefantasy/core/bungee/misc/punish/listener/PunishListener.kt
@@ -19,6 +19,10 @@ class PunishListener : Listener {
         event: PreLoginEvent
     ) {
         val connection = event.connection
+
+        println(connection.name)
+        println(connection.uniqueId)
+
         val userId = connection.uniqueId
         val user = CoreProvider.Cache.Local.USERS.provide().fetchById(userId)
         val userPunishments = user?.getPunishments() ?: emptyList()
