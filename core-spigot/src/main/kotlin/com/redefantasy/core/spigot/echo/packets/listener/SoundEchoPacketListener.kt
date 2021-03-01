93bb7ed6b82e30031edc0577d9f4abe171701b36
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-01 01:22:21 -0300

    dev

--- core-spigot/src/main/kotlin/com/redefantasy/core/spigot/echo/packets/listener/SoundEchoPacketListener.kt
+++ core-spigot/src/main/kotlin/com/redefantasy/core/spigot/echo/packets/listener/SoundEchoPacketListener.kt
@@ -1,12 +1,8 @@
 package com.redefantasy.core.spigot.echo.packets.listener
 
-import com.google.common.base.Enums
 import com.redefantasy.core.shared.echo.api.listener.EchoListener
 import com.redefantasy.core.shared.echo.packets.SoundPacket
-import net.minecraft.server.v1_8_R3.PacketPlayOutNamedSoundEffect
 import org.bukkit.Bukkit
-import org.bukkit.Sound
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer
 import org.greenrobot.eventbus.Subscribe
 
 /**
@@ -23,32 +19,12 @@ class SoundEchoPacketListener : EchoListener {
         packet.usersId?.forEach {
             val player = Bukkit.getPlayer(it)
 
-            println(it)
-            println(player === null)
-
-            if (player !== null) {
-                println("Tocar pra ele")
-
-                println(sound.name)
-
-                val _sound = Enums.getIfPresent(Sound::class.java, sound.name)
-
-                println("Dps -> ${_sound.isPresent}")
-
-                val _packet = PacketPlayOutNamedSoundEffect(
-                    sound.name,
-                    0.0,
-                    60.0,
-                    0.0,
-                    0f,
-                    0f
-                )
-
-                val handle = (player as CraftPlayer).handle
-                val playerConnection = handle.playerConnection
-
-                playerConnection.sendPacket(_packet)
-            }
+            if (player !== null) player.playSound(
+                player.location,
+                sound.name,
+                packet.volume2!!,
+                packet.volume1!!
+            )
         }
     }
 
