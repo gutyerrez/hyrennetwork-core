cfa810b217c68871356322e5525059fc5caeceff
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-01 01:02:36 -0300

    dev

--- core-shared/src/main/kotlin/com/redefantasy/core/shared/echo/packets/SoundPacket.kt
+++ core-shared/src/main/kotlin/com/redefantasy/core/shared/echo/packets/SoundPacket.kt
@@ -5,6 +5,7 @@ import com.redefantasy.core.shared.echo.api.buffer.EchoBufferOutput
 import com.redefantasy.core.shared.echo.api.packets.EchoPacket
 import com.redefantasy.core.shared.misc.utils.Sound
 import java.util.*
+import java.util.stream.Collectors
 
 /**
  * @author Gutyerrez
@@ -14,20 +15,23 @@ class SoundPacket : EchoPacket() {
     var sound: Sound? = null
     var volume1: Float? = null
     var volume2: Float? = null
-    var usersId: Array<UUID>? = null
+    var usersId: List<UUID>? = null
 
     override fun write(buffer: EchoBufferOutput) {
         buffer.writeEnum(this.sound)
         buffer.writeFloat(this.volume1)
         buffer.writeFloat(this.volume2)
-        buffer.writeArray(this.usersId)
+        buffer.writeList(this.usersId)
     }
 
     override fun read(buffer: EchoBufferInput) {
         this.sound = buffer.readEnum(Sound::class)
         this.volume1 = buffer.readFloat()
         this.volume2 = buffer.readFloat()
-        this.usersId = buffer.readArray()
+        this.usersId = buffer.readList<String>()
+            ?.stream()
+            ?.map { UUID.fromString(it) }
+            ?.collect(Collectors.toList())
     }
 
 }
