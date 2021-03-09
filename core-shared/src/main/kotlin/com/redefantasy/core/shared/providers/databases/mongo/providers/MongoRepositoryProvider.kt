8cab35d8af295388ab5eea6ed6625a833c46f2c7
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-09 04:00:19 -0300

    dev

--- core-shared/src/main/kotlin/com/redefantasy/core/shared/providers/databases/mongo/providers/MongoRepositoryProvider.kt
+++ core-shared/src/main/kotlin/com/redefantasy/core/shared/providers/databases/mongo/providers/MongoRepositoryProvider.kt
@@ -1,21 +1,24 @@
 package com.redefantasy.core.shared.providers.databases.mongo.providers
 
 import com.redefantasy.core.shared.providers.IProvider
+import com.redefantasy.core.shared.providers.databases.mongo.MongoDatabaseProvider
 import com.redefantasy.core.shared.storage.repositories.IRepository
 import kotlin.reflect.KClass
-import kotlin.reflect.full.createInstance
 
 /**
  * @author SrGutyerrez
  **/
-class MongoRepositoryProvider<T: IRepository>(
-        private val repositoryClass: KClass<out T>
+class MongoRepositoryProvider<T : IRepository>(
+    private val mongoDatabaseProvider: MongoDatabaseProvider,
+    private val repositoryClass: KClass<out T>
 ) : IProvider<T> {
 
     private lateinit var t: T
 
     override fun prepare() {
-        this.t = this.repositoryClass.createInstance()
+        this.t = this.repositoryClass.constructors
+            .first()
+            .call(this.mongoDatabaseProvider)
     }
 
     override fun provide() = this.t
