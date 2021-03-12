a5e7947714a166a95e0b2ebf69e917754ea3f5b0
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-12 09:12:17 -0300

    dev

--- core-bungee/src/main/kotlin/com/redefantasy/core/bungee/echo/packet/listener/ConnectUserToApplicationEchoPacketListener.kt
+++ core-bungee/src/main/kotlin/com/redefantasy/core/bungee/echo/packet/listener/ConnectUserToApplicationEchoPacketListener.kt
@@ -3,7 +3,6 @@ package com.redefantasy.core.bungee.echo.packet.listener
 import com.redefantasy.core.shared.CoreProvider
 import com.redefantasy.core.shared.echo.api.listener.EchoListener
 import com.redefantasy.core.shared.echo.packets.ConnectUserToApplicationPacket
-import net.md_5.bungee.BungeeServerInfo
 import net.md_5.bungee.api.ProxyServer
 import org.greenrobot.eventbus.Subscribe
 
@@ -29,13 +28,7 @@ class ConnectUserToApplicationEchoPacketListener : EchoListener {
 
         println(">> ${application.address}")
 
-        val bungeeServerInfo = BungeeServerInfo(application.address)
-
-        println(bungeeServerInfo)
-
-        println("-> ${bungeeServerInfo.address}")
-
-        proxiedPlayer.connect(bungeeServerInfo)
+        proxiedPlayer.connect { application.address }
     }
 
 }
