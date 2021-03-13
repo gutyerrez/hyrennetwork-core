88bae8737f60e88aacb52df3f58792b8f3c83b5a
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-13 10:03:17 -0300

    fix

--- core-shared/src/main/kotlin/com/redefantasy/core/shared/users/data/User.kt
+++ core-shared/src/main/kotlin/com/redefantasy/core/shared/users/data/User.kt
@@ -175,7 +175,7 @@ open class User(
 
         return groups
             .stream()
-            .sorted { o1, o2 -> o2.priority!!.compareTo(o1.priority!!) }
+            .sorted { o1, o2 -> o1.priority!!.compareTo(o2.priority!!) }
             .findFirst()
             .orElse(Group.DEFAULT)
     }
