ca196a014b596081d3ebfa9e3b7a04989ac17b0c
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-15 10:41:20 -0300

    dev

--- core-bungee/src/main/kotlin/com/redefantasy/core/bungee/CoreBungeePlugin.kt
+++ core-bungee/src/main/kotlin/com/redefantasy/core/bungee/CoreBungeePlugin.kt
@@ -42,6 +42,8 @@ class CoreBungeePlugin : CustomPlugin(true) {
         AsyncScheduler.scheduleAsyncRepeatingTask(
             {
                 ProxyServer.getInstance().players.forEach {
+                    println("Iniciando criação do status do usuário ${it.uniqueId}")
+
                     var user = CoreProvider.Cache.Local.USERS.provide().fetchById(it.uniqueId)
 
                     if (user === null) user = User(
@@ -61,8 +63,8 @@ class CoreBungeePlugin : CustomPlugin(true) {
                     )
                 }
             },
-            0,
-            5,
+            3,
+            3,
             TimeUnit.SECONDS
         )
     }
