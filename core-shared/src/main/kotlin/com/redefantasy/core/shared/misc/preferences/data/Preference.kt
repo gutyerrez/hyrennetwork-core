77d23846c51a961c20dd92c5510d22efa0ee9a6a
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-19 09:39:18 -0300

    dev

--- core-shared/src/main/kotlin/com/redefantasy/core/shared/misc/preferences/data/Preference.kt
+++ core-shared/src/main/kotlin/com/redefantasy/core/shared/misc/preferences/data/Preference.kt
@@ -1,15 +1,16 @@
 package com.redefantasy.core.shared.misc.preferences.data
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.redefantasy.core.shared.misc.preferences.PreferenceState
 
 /**
  * @author SrGutyerrez
  **/
 abstract class Preference(
-    val name: String,
+    @JsonProperty val name: String,
 ) {
 
-    var preferenceState: PreferenceState = PreferenceState.ENABLED
+    @JsonProperty var preferenceState: PreferenceState = PreferenceState.ENABLED
 
     abstract fun <T> getIcon(): T
 
