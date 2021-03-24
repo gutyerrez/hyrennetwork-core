1d5970501d58e7bf1d4e21f49f7853152a9517b3
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-24 19:38:39 -0300

    dev

--- core-shared/src/main/kotlin/com/redefantasy/core/shared/misc/preferences/Preference.kt
+++ core-shared/src/main/kotlin/com/redefantasy/core/shared/misc/preferences/Preference.kt
@@ -1,17 +1,20 @@
 package com.redefantasy.core.shared.misc.preferences
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.redefantasy.core.shared.misc.minecraft.material.Material
 import java.io.Serializable
 
 /**
  * @author SrGutyerrez
  **/
-interface Preference : Serializable {
+abstract class Preference(
+    @JsonProperty
+    open val name: String,
+    @JsonProperty("preference_state")
+    var preferenceState: PreferenceState = PreferenceState.ENABLED
+) : Serializable {
 
-    val name: String
-    var preferenceState: PreferenceState
-
-    fun getIcon(): PreferenceIcon
+    abstract fun getIcon(): PreferenceIcon
 
 }
 
--- core-shared/src/main/kotlin/com/redefantasy/core/shared/misc/preferences/data/TellPreference.kt
+++ core-shared/src/main/kotlin/com/redefantasy/core/shared/misc/preferences/data/TellPreference.kt
@@ -1,20 +1,15 @@
 package com.redefantasy.core.shared.misc.preferences.data
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.redefantasy.core.shared.misc.minecraft.material.Material
 import com.redefantasy.core.shared.misc.preferences.Preference
 import com.redefantasy.core.shared.misc.preferences.PreferenceIcon
-import com.redefantasy.core.shared.misc.preferences.PreferenceState
 
 /**
  * @author Gutyerrez
  */
 data class TellPreference(
-    @JsonProperty
-    override val name: String = "user-private-messages-preference",
-    @JsonProperty("preference_state")
-    override var preferenceState: PreferenceState = PreferenceState.ENABLED
-) : Preference {
+    override val name: String = "user-private-messages-preference"
+) : Preference(name) {
 
     override fun getIcon() = PreferenceIcon(
         Material.EMPTY_MAP,
