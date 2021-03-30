ef750a5ec08ae8c846fea7de0b5db3ed0fdc03dc
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-30 02:22:16 -0300

    dev

---
+++ core-spigot/src/main/kotlin/com/redefantasy/core/spigot/misc/minecraft/entity/exceptions/EntitySpawnException.kt
@@ -0,0 +1,8 @@
+package com.redefantasy.core.spigot.misc.minecraft.entity.exceptions
+
+/**
+ * @author Gutyerrez
+ */
+class EntitySpawnException(
+    message: String
+) : RuntimeException(message)
