845e28d3b5179aac9655b69d0023d59be473612e
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-30 06:54:38 -0300

    dev

---
+++ core-spigot/src/main/kotlin/com/redefantasy/core/spigot/misc/hologram/Hologram.kt
@@ -0,0 +1,84 @@
+package com.redefantasy.core.spigot.misc.hologram
+
+import com.redefantasy.core.spigot.misc.hologram.lines.HologramLine
+import org.bukkit.Location
+import java.util.stream.Collectors
+
+/**
+ * @author Gutyerrez
+ */
+class Hologram(
+    _lines: List<String>,
+    private val hologramPosition: HologramPosition
+) {
+
+    private val lines = mutableListOf<HologramLine>()
+
+    init {
+        this.lines.addAll(
+            _lines.stream().map { HologramLine(it) }.collect(Collectors.toList())
+        )
+    }
+
+    fun line(text: String): Hologram {
+        this.lines.add(
+            HologramLine(text)
+        )
+
+        return this
+    }
+
+    fun line(
+        position: Int,
+        text: String
+    ): Hologram {
+        this.lines[position] = HologramLine(text)
+
+        return this
+    }
+
+    fun update(
+        position: Int,
+        text: String
+    ) {
+        this.lines[position].update(text)
+    }
+
+    fun update() {
+        this.lines.forEach { it.update() }
+    }
+
+    fun isSpawned() = this.lines.stream().allMatch { it.isSpawned() }
+
+    fun spawn(location: Location) {
+        if (!location.chunk.isLoaded) location.chunk.load()
+
+        var hologramLocation = location.clone()
+
+        this.lines.forEach {
+            it.spawn(hologramLocation)
+
+            hologramLocation = hologramLocation.add(0.0, this.hologramPosition.value, 0.0)
+        }
+    }
+
+    fun destroy() = this.lines.forEach { it.destroy() }
+
+    fun teleport(location: Location) {
+        var hologramLocation = location.clone()
+
+        this.lines.forEach {
+            it.teleport(hologramLocation)
+
+            hologramLocation = hologramLocation.add(0.0, this.hologramPosition.value, 0.0)
+        }
+    }
+
+    enum class HologramPosition(val value: Double) {
+
+        UP(0.23),
+        DOWN(-0.29)
+
+    }
+
+}
