f44303962775965e91ba24b476929e1b0791db8b
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-31 01:13:31 -0300

    dev

--- core-spigot/src/main/kotlin/com/redefantasy/core/spigot/misc/frame/data/Frame.kt
+++ core-spigot/src/main/kotlin/com/redefantasy/core/spigot/misc/frame/data/Frame.kt
@@ -69,8 +69,8 @@ data class Frame(val url: URL) {
         this.id = UUID.nameUUIDFromBytes(("Frame:${RandomStringUtils.random(16)}").toByteArray(Charsets.UTF_8))
         this.bufferedImage = FrameUtils.resize(
             bufferedImage,
-            xPanes * 128,
-            yPanes * 128
+            xPanes * 100,
+            yPanes * 100
         )
         this.lengthX = xPanes
         this.lengthY = yPanes
@@ -99,7 +99,7 @@ data class Frame(val url: URL) {
         y: Int,
         mapView: MapView
     ) {
-        val bufferedImage = this.bufferedImage.getSubimage(x * 128, y * 128, 128, 128)
+        val bufferedImage = this.bufferedImage.getSubimage(x * 100, y * 100, 100, 100)
 
         mapView.renderers.forEach { mapView.removeRenderer(it) }
 
