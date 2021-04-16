9929b48d9eaa904ad0fac15bb18a742bb43fa8c9
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-16 10:25:29 -0300

    dev

--- core-spigot/src/main/kotlin/com/redefantasy/core/spigot/CoreSpigotConstants.kt
+++ core-spigot/src/main/kotlin/com/redefantasy/core/spigot/CoreSpigotConstants.kt
@@ -1,5 +1,7 @@
 package com.redefantasy.core.spigot
 
+import com.redefantasy.core.shared.CoreProvider
+import com.redefantasy.core.shared.servers.ServerType
 import com.redefantasy.core.spigot.misc.utils.ProtocolHandler
 import com.redefantasy.core.spigot.world.location.parser.BukkitLocationParser
 
@@ -10,6 +12,8 @@ object CoreSpigotConstants {
 
     val BUKKIT_LOCATION_PARSER = BukkitLocationParser()
 
-    val PROTOCOL_HANDLER = ProtocolHandler()
+    val PROTOCOL_HANDLER = if (CoreProvider.application.server?.serverType == ServerType.FACTIONS) {
+        null
+    } else ProtocolHandler()
 
 }
