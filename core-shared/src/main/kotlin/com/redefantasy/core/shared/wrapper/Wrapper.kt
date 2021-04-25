9d3c9c037c927af0a378040e850aed2fedd98c75
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-25 19:12:11 -0300

    debug

--- core-shared/src/main/kotlin/com/redefantasy/core/shared/wrapper/Wrapper.kt
+++ core-shared/src/main/kotlin/com/redefantasy/core/shared/wrapper/Wrapper.kt
@@ -7,11 +7,19 @@ import net.md_5.bungee.api.chat.BaseComponent
  */
 interface Wrapper<T> {
 
-    fun sendMessage(senderName: String, message: String)
+    fun sendMessage(
+        senderName: String,
+        message: String
+    )
 
-    fun sendMessage(senderName: String, message: BaseComponent)
+    fun sendMessage(
+        senderName: String,
+        message: BaseComponent
+    )
 
-    fun sendMessage(senderName: String, messages: Array<BaseComponent>)
-
+    fun sendMessage(
+        senderName: String,
+        messages: Array<BaseComponent>
+    )
 
 }
