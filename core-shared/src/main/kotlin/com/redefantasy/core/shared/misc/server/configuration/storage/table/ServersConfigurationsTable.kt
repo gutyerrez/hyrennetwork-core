c94ec5f9fdc1b02b6c63b38305a1489200f43f8f
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-26 18:12:15 -0300

    dev

--- core-shared/src/main/kotlin/com/redefantasy/core/shared/misc/server/configuration/storage/table/ServersConfigurationsTable.kt
+++ core-shared/src/main/kotlin/com/redefantasy/core/shared/misc/server/configuration/storage/table/ServersConfigurationsTable.kt
@@ -2,15 +2,15 @@ package com.redefantasy.core.shared.misc.server.configuration.storage.table
 
 import com.redefantasy.core.shared.misc.exposed.json
 import com.redefantasy.core.shared.misc.server.configuration.data.ServerConfiguration
-import com.redefantasy.core.shared.providers.databases.postgres.dao.StringTable
 import com.redefantasy.core.shared.servers.storage.table.ServersTable
+import org.jetbrains.exposed.sql.Table
 
 /**
  * @author Gutyerrez
  */
-object ServersConfigurationsTable : StringTable("servers_configurations") {
+object ServersConfigurationsTable : Table("servers_configurations") {
 
-	val server = reference("server", ServersTable)
+	val server = reference("name", ServersTable)
 	val configuration = json<ServerConfiguration<Any>>(
 		"configuration",
 		ServerConfiguration::class
