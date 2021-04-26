756c4cb81118a2c938dc6478cc0aa7ddd3d638ec
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-26 18:19:13 -0300

    dev

--- core-shared/src/main/kotlin/com/redefantasy/core/shared/misc/server/configuration/data/ServerConfiguration.kt
+++ core-shared/src/main/kotlin/com/redefantasy/core/shared/misc/server/configuration/data/ServerConfiguration.kt
@@ -1,18 +1,11 @@
 package com.redefantasy.core.shared.misc.server.configuration.data
 
-import com.redefantasy.core.shared.CoreProvider
 import com.redefantasy.core.shared.misc.server.configuration.settings.ServerSettings
-import org.jetbrains.exposed.dao.id.EntityID
 
 /**
  * @author Gutyerrez
  */
 data class ServerConfiguration<T>(
-	val serverName: EntityID<String>,
 	val settings: ServerSettings,
 	val icon: T? = null
-) {
-
-	val server = CoreProvider.Cache.Local.SERVERS.provide().fetchByName(this.serverName)
-
-}
+)
