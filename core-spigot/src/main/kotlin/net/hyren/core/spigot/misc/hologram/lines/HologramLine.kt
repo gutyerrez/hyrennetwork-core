fc43c2f3b3e209ed81b8d3586e0b16de3485f53a
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-05-10 10:09:36 -0300

    dev

--- core-spigot/src/main/kotlin/net/hyren/core/spigot/misc/hologram/lines/HologramLine.kt
+++ core-spigot/src/main/kotlin/net/hyren/core/spigot/misc/hologram/lines/HologramLine.kt
@@ -1,9 +1,7 @@
 package net.hyren.core.spigot.misc.hologram.lines
 
-import net.hyren.core.spigot.CoreSpigotPlugin
-import net.hyren.core.spigot.misc.player.sendPacket
-import net.minecraft.server.v1_8_R3.*
-import org.bukkit.Bukkit
+import net.minecraft.server.v1_8_R3.EntityArmorStand
+import net.minecraft.server.v1_8_R3.WorldServer
 import org.bukkit.Location
 import org.bukkit.craftbukkit.v1_8_R3.CraftWorld
 
@@ -16,30 +14,15 @@ class HologramLine(
 
     private lateinit var entityArmorStand: EntityArmorStand
 
+    private lateinit var _worldServer: WorldServer
+
     fun update(text: String) {
         this.text = text
 
         this.update()
     }
 
-    fun update() {
-        if (isSpawned()) {
-            val dataWatcher = entityArmorStand.dataWatcher
-
-            dataWatcher.watch(2, this.text)
-            dataWatcher.watch(3, 1.toByte())
-
-            val packet = PacketPlayOutEntityMetadata(entityArmorStand.id, dataWatcher, true)
-
-            Bukkit.getOnlinePlayers().forEach { it.sendPacket(packet) }
-
-            println("Atualizar linhas")
-
-            println(entityArmorStand.locX)
-            println(entityArmorStand.locY)
-            println(entityArmorStand.locZ)
-        }
-    }
+    fun update() = if (isSpawned()) entityArmorStand.customName = text else Unit
 
     fun isSpawned() = this::entityArmorStand.isInitialized && !entityArmorStand.dead
 
@@ -61,32 +44,11 @@ class HologramLine(
 
         worldServer.addEntity(entityArmorStand)
 
-        val packet = PacketPlayOutSpawnEntityLiving(entityArmorStand)
-
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(CoreSpigotPlugin.instance, {
-            Bukkit.getOnlinePlayers().forEach { it.sendPacket(packet) }
-        }, 20, 20)
-
         this.update()
     }
 
-    fun destroy() {
-        val packet = PacketPlayOutEntityDestroy(entityArmorStand.id)
+    fun destroy() = this._worldServer.removeEntity(this.entityArmorStand)
 
-        Bukkit.getOnlinePlayers().forEach { player -> player.sendPacket(packet) }
-    }
+    fun teleport(location: Location) = this.entityArmorStand.teleportTo(location, false)
 
-    fun teleport(location: Location) {
-        val packet = PacketPlayOutEntityTeleport(
-            entityArmorStand.id,
-            MathHelper.floor(location.x * 32.0),
-            MathHelper.floor(location.y * 32.0),
-            MathHelper.floor(location.z * 32.0),
-            (location.yaw * 256.0f / 360.0f).toInt().toByte(),
-            (location.yaw * 256.0f / 360.0f).toInt().toByte(),
-            false
-        )
-
-        Bukkit.getOnlinePlayers().forEach { player -> player.sendPacket(packet) }
-    }
 }
