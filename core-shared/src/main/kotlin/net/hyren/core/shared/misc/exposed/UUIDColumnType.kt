caa228b5147e9312f0229db26561be496a404e79
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-05-13 12:21:14 -0300

    debug

--- core-shared/src/main/kotlin/net/hyren/core/shared/misc/exposed/UUIDColumnType.kt
+++ core-shared/src/main/kotlin/net/hyren/core/shared/misc/exposed/UUIDColumnType.kt
@@ -64,6 +64,5 @@ class UUIDColumnType : ColumnType() {
     companion object {
         private val uuidRegexp = Regex("[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}", RegexOption.IGNORE_CASE)
     }
-}
 
 }
