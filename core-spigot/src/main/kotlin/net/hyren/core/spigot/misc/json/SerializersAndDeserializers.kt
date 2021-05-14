9a9fdf4752ef7d445b7d3d73ee8dc24c9260ce24
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-05-14 20:02:17 -0300

    debug

--- core-spigot/src/main/kotlin/net/hyren/core/spigot/misc/json/SerializersAndDeserializers.kt
+++ core-spigot/src/main/kotlin/net/hyren/core/spigot/misc/json/SerializersAndDeserializers.kt
@@ -6,10 +6,8 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.*
-import kotlinx.serialization.serializer
 import net.hyren.core.spigot.misc.server.configuration.settings.ServerSettings
 import org.bukkit.inventory.ItemStack
-import kotlin.reflect.full.starProjectedType
 
 /**
  * @author Gutyerrez
@@ -36,12 +34,19 @@ object ItemStackSerializer : KSerializer<ItemStack> {
 		val deserializedValues = mutableMapOf<String, Any>()
 
 		serializedItemStack.entries.forEach {
+			if (it.value !is JsonPrimitive) return@forEach
+
 			val _key = it.key
-			val _value = it.value
+			val _value = it.value.jsonPrimitive
 
-			val element = Json.encodeToJsonElement(serializer(_value::class.starProjectedType), _value)
-
-			deserializedValues[_key] = element
+			when {
+				_value.intOrNull != null -> deserializedValues[_key] = _value.int
+				_value.doubleOrNull != null -> deserializedValues[_key] = _value.double
+				_value.floatOrNull != null -> deserializedValues[_key] = _value.float
+				_value.longOrNull != null -> deserializedValues[_key] = _value.long
+				_value.booleanOrNull != null -> deserializedValues[_key] = _value.boolean
+				_value.isString -> deserializedValues[_key] = _value.toString()
+			}
 		}
 
 		println(deserializedValues)
