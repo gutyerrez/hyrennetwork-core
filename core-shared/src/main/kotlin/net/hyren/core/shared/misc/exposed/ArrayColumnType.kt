6ad9028aecbbeb365b00bb11e03f362e8005f7de
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-05-14 18:31:24 -0300

    dev

--- core-shared/src/main/kotlin/net/hyren/core/shared/misc/exposed/ArrayColumnType.kt
+++ core-shared/src/main/kotlin/net/hyren/core/shared/misc/exposed/ArrayColumnType.kt
@@ -5,8 +5,11 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonObject
+import net.hyren.core.shared.CoreProvider
+import net.hyren.core.shared.applications.ApplicationType
+import net.hyren.core.shared.misc.punish.durations.PunishDuration
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.ColumnType
 import org.jetbrains.exposed.sql.Table
@@ -18,11 +21,19 @@ import kotlin.reflect.KClass
  * @author Gutyerrez
  */
 inline fun <reified T> Table.array(
-    name: String
-): Column<Array<T>> = registerColumn(name, ArrayColumnType<T>(T::class))
+    name: String,
+    noinline deserialize: (decoder: Decoder) -> Array<T>? = { null }
+): Column<Array<T>> = registerColumn(
+    name,
+    ArrayColumnType(
+        T::class,
+        deserialize
+    )
+)
 
 class ArrayColumnType<T>(
-    private val kClass: KClass<*>
+    private val kClass: KClass<*>,
+    private val deserialize: (decoder: Decoder) -> Array<T>?
 ) : ColumnType() {
 
     override fun sqlType() = "longtext"
@@ -39,36 +50,41 @@ class ArrayColumnType<T>(
 
             return value.array
         } else if (value is String) {
-            return Json.decodeFromString(object : DeserializationStrategy<Array<T>> {
-                override val descriptor: SerialDescriptor = ContextualSerializer(
-                    kClass,
-                    null,
-                    emptyArray()
-                ).descriptor
+            return if (CoreProvider.application.applicationType != ApplicationType.SERVER_TESTS) {
+                Json.decodeFromString(object : DeserializationStrategy<Array<T>> {
+                    override val descriptor: SerialDescriptor = ContextualSerializer(
+                        kClass,
+                        null,
+                        emptyArray()
+                    ).descriptor
 
-                override fun deserialize(
-                    decoder: Decoder
-                ): Array<T> {
-                    val jsonArray = (decoder as JsonDecoder).decodeJsonElement().jsonArray
+                    override fun deserialize(
+                        decoder: Decoder
+                    ): Array<T> {
+                        val jsonArray = (decoder as JsonDecoder).decodeJsonElement().jsonArray
 
-                    val array = java.lang.reflect.Array.newInstance(
-                        kClass.java,
-                        jsonArray.size
-                    ) as Array<T>
+                        val array = java.lang.reflect.Array.newInstance(
+                            kClass.java,
+                            jsonArray.size
+                        ) as Array<T>
 
-                    var _index = 0
+                        var _index = 0
 
-                    jsonArray.forEachIndexed { index, jsonElement ->
-                        val a = jsonElement.jsonObject as T
+                        jsonArray.forEachIndexed { index, jsonElement ->
+                            when (kClass) {
+                                PunishDuration::class -> {
+                                    array[_index] = Json.decodeFromJsonElement<PunishDuration>(jsonElement) as T
+                                }
+                                else -> this@ArrayColumnType.deserialize.invoke(decoder)
+                            }
 
-                        println(a)
+                            _index++
+                        }
 
-                        _index++
+                        return array
                     }
-
-                    return array
-                }
-            }, value)
+                }, value)
+            } else return Any()
         } else if (value is Array<*>) {
             return value
         }
