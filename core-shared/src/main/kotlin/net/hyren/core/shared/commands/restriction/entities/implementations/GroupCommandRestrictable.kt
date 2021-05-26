fd28b3c1fe774585e0d7be82e231b03ece4f20bf
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-05-26 01:40:25 -0300

    dev

--- core-shared/src/main/kotlin/net/hyren/core/shared/commands/restriction/entities/implementations/GroupCommandRestrictable.kt
+++ core-shared/src/main/kotlin/net/hyren/core/shared/commands/restriction/entities/implementations/GroupCommandRestrictable.kt
@@ -1,8 +1,8 @@
 package net.hyren.core.shared.commands.restriction.entities.implementations
 
-import net.hyren.core.shared.CoreProvider
 import net.hyren.core.shared.commands.restriction.entities.CommandRestrictable
 import net.hyren.core.shared.groups.Group
+import net.hyren.core.shared.servers.data.Server
 import net.hyren.core.shared.users.data.User
 import net.md_5.bungee.api.chat.ComponentBuilder
 
@@ -11,24 +11,26 @@ import net.md_5.bungee.api.chat.ComponentBuilder
  **/
 interface GroupCommandRestrictable : CommandRestrictable {
 
+    fun getServer(): Server? = null
+
     fun getGroup(): Group
 
     fun strictGroup(): Boolean = false
 
     override fun canExecute(user: User?): Boolean {
-        if (user == null) return false
+        if (user == null) {
+            return false
+        }
 
-        val server = CoreProvider.application.server
-
-        return if (this.strictGroup()) {
-            user.hasStrictGroup(this.getGroup(), server)
+        return if (strictGroup()) {
+            user.hasStrictGroup(getGroup(), getServer())
         } else {
-            user.hasGroup(this.getGroup(), server)
+            user.hasGroup(getGroup(), getServer())
         }
     }
 
-    override fun getErrorMessage() = ComponentBuilder()
-        .appendLegacy("§cÉ necessário o grupo ${this.getGroup().displayName} ou superior para executar este comando.")
-        .create()
+    override fun getErrorMessage() = ComponentBuilder(
+        "§cÉ necessário o grupo ${getGroup().displayName} ou superior para executar este comando."
+    ).create()
 
 }
