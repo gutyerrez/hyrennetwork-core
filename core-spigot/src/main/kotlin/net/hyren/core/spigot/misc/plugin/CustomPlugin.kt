30baedf5b28d1652e81d700b795122b4e31d6df0
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-07-23 01:50:10 -0300

    dev

--- core-spigot/src/main/kotlin/net/hyren/core/spigot/misc/plugin/CustomPlugin.kt
+++ core-spigot/src/main/kotlin/net/hyren/core/spigot/misc/plugin/CustomPlugin.kt
@@ -1,7 +1,9 @@
 package net.hyren.core.spigot.misc.plugin
 
 import net.hyren.core.shared.CoreProvider
+import net.minecraft.server.v1_8_R3.DedicatedServer
 import org.bukkit.Bukkit
+import org.bukkit.World
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
@@ -21,6 +23,12 @@ abstract class CustomPlugin(
                 runnable.run()
             }
         }
+    }
+
+    fun getDefaultWorld(): World {
+        return Bukkit.getWorld(
+            DedicatedServer.getServer().propertyManager.getString("level-name", "world")
+        )
     }
 
 }
