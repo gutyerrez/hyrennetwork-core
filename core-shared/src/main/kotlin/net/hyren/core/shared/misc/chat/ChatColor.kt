45aa7e4c56badc6af4e9c2e12ab942eb9dd9868a
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-07-23 13:14:02 -0300

    dev

--- core-shared/src/main/kotlin/net/hyren/core/shared/misc/chat/ChatColor.kt
+++ core-shared/src/main/kotlin/net/hyren/core/shared/misc/chat/ChatColor.kt
@@ -1,8 +1,14 @@
 package net.hyren.core.shared.misc.chat
 
+import net.md_5.bungee.api.chat.BaseComponent
+
 /**
  * @author Gutyerrez
  */
 operator fun net.md_5.bungee.api.ChatColor.plus(other: String) = this.toString() + other
+
+operator fun Array<net.md_5.bungee.api.chat.BaseComponent>.plus(
+    other: String
+) = BaseComponent.toLegacyText(*this) + other
 
 operator fun net.hyren.core.shared.misc.utils.ChatColor.plus(other: String) = this.toString() + other
