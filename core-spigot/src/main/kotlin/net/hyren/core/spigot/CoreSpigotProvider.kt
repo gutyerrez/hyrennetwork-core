692527f9ee1f36e381d0a20ee97237a1a2788497
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-07-24 01:53:26 -0300

    dev

--- core-spigot/src/main/kotlin/net/hyren/core/spigot/CoreSpigotProvider.kt
+++ core-spigot/src/main/kotlin/net/hyren/core/spigot/CoreSpigotProvider.kt
@@ -4,16 +4,7 @@ import kotlinx.serialization.json.JsonDecoder
 import kotlinx.serialization.json.JsonEncoder
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
-import net.hyren.core.shared.misc.json.KJson
-import net.hyren.core.shared.misc.json.KSerializer
-import net.hyren.core.shared.misc.json.asBoolean
-import net.hyren.core.shared.misc.json.asDouble
-import net.hyren.core.shared.misc.json.asEnum
-import net.hyren.core.shared.misc.json.asFloat
-import net.hyren.core.shared.misc.json.asInt
-import net.hyren.core.shared.misc.json.asJsonArray
-import net.hyren.core.shared.misc.json.asJsonObject
-import net.hyren.core.shared.misc.json.asString
+import net.hyren.core.shared.misc.json.*
 import net.hyren.core.shared.providers.IProvider
 import net.hyren.core.shared.providers.cache.local.LocalCacheProvider
 import net.hyren.core.shared.providers.databases.postgresql.providers.PostgreSQLRepositoryProvider
@@ -115,23 +106,23 @@ object CoreSpigotProvider {
 
                                 return ServerConfiguration(
                                     ServerSettings(
-                                        jsonObject.getValue("server_settings").asJsonObject().getValue("max_players").asInt(),
-                                        jsonObject.getValue("server_settings").asJsonObject().getValue("view_distance").asInt(),
+                                        jsonObject.getValue("settings").asJsonObject().getValue("max_players").asInt(),
+                                        jsonObject.getValue("settings").asJsonObject().getValue("view_distance").asInt(),
                                         SerializedLocation(
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("world_name").asString(),
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("x").asDouble(),
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("y").asDouble(),
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("z").asDouble(),
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("yaw").asFloat(),
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("pitch").asFloat(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("world_name").asString(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("x").asDouble(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("y").asDouble(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("z").asDouble(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("yaw").asFloat(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("spawn_location").asJsonObject().getValue("pitch").asFloat(),
                                         ),
                                         SerializedLocation(
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("world_name").asString(),
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("x").asDouble(),
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("y").asDouble(),
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("z").asDouble(),
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("yaw").asFloat(),
-                                            jsonObject.getValue("server_settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("pitch").asFloat(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("world_name").asString(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("x").asDouble(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("y").asDouble(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("z").asDouble(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("yaw").asFloat(),
+                                            jsonObject.getValue("settings").asJsonObject().getValue("npc_location").asJsonObject().getValue("pitch").asFloat(),
                                         )
                                     ),
                                     ItemBuilder(
