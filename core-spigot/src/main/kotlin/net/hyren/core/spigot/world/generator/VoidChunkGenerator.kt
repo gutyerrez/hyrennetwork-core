7a8bde618394046f4ce310c787810b9fbf89284b
Vinicius Gutierrez <srgutyerrez@gmail.com>
2021-07-29 04:42:04 -0300

    dev

--- core-spigot/src/main/kotlin/net/hyren/core/spigot/world/generator/VoidChunkGenerator.kt
+++ core-spigot/src/main/kotlin/net/hyren/core/spigot/world/generator/VoidChunkGenerator.kt
@@ -1,14 +1,25 @@
 package net.hyren.core.spigot.world.generator
 
 import org.bukkit.Location
-import org.bukkit.Material
 import org.bukkit.World
-import org.bukkit.block.Biome
+import org.bukkit.generator.BlockPopulator
 import org.bukkit.generator.ChunkGenerator
 import java.util.*
 
 class VoidChunkGenerator : ChunkGenerator() {
 
+    override fun getDefaultPopulators(
+        world: World
+    ) = emptyList<BlockPopulator>()
+
+    override fun generate(
+        world: World,
+        random: Random,
+        x: Int,
+        z: Int
+    ) = ByteArray(32768)
+
+    /*
     override fun generateChunkData(
         world: World,
         random: Random,
@@ -30,7 +41,7 @@ class VoidChunkGenerator : ChunkGenerator() {
 
         return chunkData
     }
-
+     */
     override fun getFixedSpawnLocation(
         world: World,
         random: Random
