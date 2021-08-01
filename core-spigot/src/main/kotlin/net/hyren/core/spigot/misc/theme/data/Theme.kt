d5e1ab96d7bc2ecaa2305c29facbaca9b0398af7
Vinicius Gutierrez <srgutyerrez@gmail.com>
2021-08-01 14:29:58 -0300

    debug

--- core-spigot/src/main/kotlin/net/hyren/core/spigot/misc/theme/data/Theme.kt
+++ core-spigot/src/main/kotlin/net/hyren/core/spigot/misc/theme/data/Theme.kt
@@ -1,14 +1,24 @@
 package net.hyren.core.spigot.misc.theme.data
 
+import kotlin.experimental.and
 import net.hyren.core.shared.CoreConstants
 import net.hyren.core.shared.CoreProvider
 import net.hyren.core.shared.applications.ApplicationType
 import net.hyren.core.shared.applications.data.Application
-import net.hyren.core.spigot.misc.utils.schematic.Clipboard
-import net.hyren.core.spigot.misc.utils.schematic.ClipboardTools
+import net.hyren.core.shared.misc.kotlin.sizedArray
+import net.hyren.core.spigot.misc.asNMSWorld
+import net.hyren.core.spigot.misc.theme.nbt.ByteArrayTag
+import net.hyren.core.spigot.misc.theme.nbt.CompoundTag
+import net.hyren.core.spigot.misc.theme.nbt.ShortTag
+import net.hyren.core.spigot.misc.theme.nbt.stream.NBTInputStream
+import net.minecraft.server.v1_8_R3.Block
+import net.minecraft.server.v1_8_R3.BlockPosition
+import net.minecraft.server.v1_8_R3.Blocks
 import org.bukkit.Bukkit
-import org.bukkit.Location
 import java.io.File
+import java.io.FileInputStream
+import java.util.zip.DataFormatException
+import java.util.zip.GZIPInputStream
 
 /**
  * @author Gutyerrez
@@ -37,19 +47,87 @@ data class Theme(
         y: Int,
         z: Int
     ) {
-        val clipboard = Clipboard(schematic)
+        FileInputStream(schematic).use {
+            val nbtInputStream = NBTInputStream(
+                GZIPInputStream(it)
+            )
 
-        ClipboardTools.binaryPaste(
-            Location(
-                Bukkit.getWorld(worldName),
-                x.toDouble(), y.toDouble(), z.toDouble()
-            ),
-            clipboard.blockIds,
-            clipboard.blockData,
-            clipboard.length.toInt(),
-            clipboard.width.toInt(),
-            clipboard.height.toInt()
-        )
+            val schematicTag = nbtInputStream.readTag() as CompoundTag
+
+            nbtInputStream.close()
+
+            if (schematicTag.name != "Schematic") {
+                throw DataFormatException("Tag \"Schematic\" does not exists or is not first")
+            }
+
+            val schematic = schematicTag.value
+
+            if (!schematic.containsKey("Blocks")) {
+                throw DataFormatException("Schematic file is missing a \"Blocks\" tag")
+            }
+
+            val width = (schematic["Width"] as ShortTag).value
+            val height = (schematic["Height"] as ShortTag).value
+            val length = (schematic["Length"] as ShortTag).value
+
+            val blocks = (schematic["Blocks"] as ByteArrayTag).value
+            val data = (schematic["Data"] as ByteArrayTag).value
+
+            var addId = ByteArray(0)
+
+            val blocksIds = sizedArray<Short>(blocks.size)
+
+            if (schematic.containsKey("AddBlocks")) {
+                addId = (schematic["AddBlocks"] as ByteArrayTag).value
+            }
+
+            for (index in blocks.indices) {
+                if ((index shr 1) >= addId.size) {
+                    blocksIds[index] = (blocks[index] and 0xFF.toByte()).toShort()
+                } else {
+                    if ((index and 1) == 0) {
+                        blocksIds[index] = (((addId[index shr 1] and 0x0F.toByte()).toInt() shl 8) + (blocks[index] and 0xFF.toByte())).toShort()
+                    } else {
+                        blocksIds[index] = (((addId[index shr 1] and 0xF0.toByte()).toInt() shl 4) + (blocks[index] and 0xFF.toByte())).toShort()
+                    }
+                }
+            }
+
+            val worldServer = Bukkit.getWorld(worldName).asNMSWorld()
+
+            for (blockX in 0 until width) {
+                for (blockY in 0 until height) {
+                    for (blockZ in 0 until length) {
+                        if (!worldServer.chunkProviderServer.isChunkLoaded(x shr 4, z shr 4)) {
+                            worldServer.chunkProviderServer.loadChunk(x shr 4, z shr 4)
+                        }
+
+                        val chunk = worldServer.getChunkAt(x shr 4, z shr 4)
+
+                        val index = blockY * width * length + blockZ * width + blockX
+
+                        val blockData = Block.getByCombinedId(blocksIds[index].toInt() + (data[index].toInt() shl 12))
+
+                        if (blockData.block == Blocks.AIR) {
+                            continue
+                        }
+
+                        if (!worldServer.chunkProviderServer.isChunkLoaded(blockX, blockZ)) {
+                            worldServer.chunkProviderServer.loadChunk(blockX, blockZ)
+                        }
+
+                        chunk.a(
+                            BlockPosition(
+                                (x + blockX),
+                                (y + blockY),
+                                (z + blockZ),
+                            ),
+                            blockData
+                        )
+                    }
+                }
+            }
+        }
     }
 
     private fun Application.getThemesFolder(): String = when (applicationType) {
